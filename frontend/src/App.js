ef24fa48b27b7dfc0decb0bb315d3c94b14e37fa
David Fischer <dfischer@fischerltd.com>
2023-12-05 14:12:16 -0500

    login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import LoginForm from './users/LoginForm'
+import CurrentUserProvider from './contexts/CurrentUser'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CurrentUserProvider>
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LoginForm} />
+          <Route path="/" component={Error404} />
+        </Switch>
+      </BrowserRouter>
+    </CurrentUserProvider>
   );
 }
 
