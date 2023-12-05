18194ca9ceb430e6267483b8bb8f7b3fe1c06f05
David Fischer <dfischer@fischerltd.com>
2023-12-05 14:23:44 -0500

    components

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react"
 import { useHistory } from "react-router"
-import { CurrentUser } from "../contexts/CurrentUser"
+import { CurrentUser } from "../../contexts/CurrentUser"
 
 function LoginForm() {
 
