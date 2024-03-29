7529e497ef916b61746d4f25ab7147f593d0acf0
David Fischer <dfischer@fischerltd.com>
2024-03-29 00:21:08 -0400

    sidebar adjustments

--- react-sidebar/src/App.js
+++ react-sidebar/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
-//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
 import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
